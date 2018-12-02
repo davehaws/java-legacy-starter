90bb5bab05b312c97b2002de6f44ddca0602ce40
Dave Haws <davehaws@users.noreply.github.com>
2018-12-01 21:39:51 -0600

    Completed the ApplicantName class. Now need to use it.

--- src/main/java/com/neopragma/legacy/screen/ApplicantName.java
+++ src/main/java/com/neopragma/legacy/screen/ApplicantName.java
@@ -4,13 +4,51 @@ public class ApplicantName {
 
 	public static final int VALID_NAME = 0;
 	public static final int INVALID_NAME = 6;
+	
+	String firstName;
+	String middleName;
+	String lastName;
 
 	public void setName(String first, String middle, String last) {
+		this.firstName = protectAgainstNull(first);
+		this.middleName = protectAgainstNull(middle);
+		this.lastName = protectAgainstNull(last);
+	}
 
+	private String protectAgainstNull(String string) {
+		return string == null ? "" : string;
+	}
+
+	public void setSpanishName(String primerNombre, String segundoNombre,
+							   String primerApellido, String segundoApellido) {
+		String lastName = null;
+		if ( primerApellido != null ) {
+  		    StringBuilder sb = new StringBuilder(primerApellido);
+  		    if (segundoApellido != null && !segundoApellido.isEmpty()) {
+  		    	sb.append(" " + segundoApellido);
+  		    }
+		    lastName = sb.toString();
+		}
+		setName(primerNombre, segundoNombre, lastName);
 	}
 
 	public int validateName() {
+		if ( firstName.isEmpty() || lastName.isEmpty() ) {
+			return INVALID_NAME;
+		}
+		
 		return VALID_NAME;
+	}
+
+	public String formatLastNameFirst() {
+		StringBuilder sb = new StringBuilder(lastName);
+		sb.append(", ");
+		sb.append(firstName);
+		if ( !middleName.isEmpty() ) {
+			sb.append(" ");
+			sb.append(middleName);
+		}
+		return sb.toString();
 	}
 
 }
--- src/test/java/com/neopragma/legacy/screen/ApplicantNameTest.java
+++ src/test/java/com/neopragma/legacy/screen/ApplicantNameTest.java
@@ -2,16 +2,77 @@ package com.neopragma.legacy.screen;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ApplicantNameTest {
 
+	ApplicantName name;
+	
+	@Before
+	public void beforeEach() {
+		name = new ApplicantName();
+	}
+	
 	@Test
 	public void completeNameProvided() {
-		ApplicantName name = new ApplicantName();
 		name.setName("First", "Middle", "Last");
-		assertEquals(0, name.validateName());
+		assertEquals(ApplicantName.VALID_NAME, name.validateName());
 	}
 
+	@Test
+	public void firstAndLastNamesProvided() {
+		name.setName("First", null, "Last");
+		assertEquals(ApplicantName.VALID_NAME, name.validateName());
+	}
+	
+	@Test
+	public void missingFirstName() {
+		name.setName(null, null, "Last");
+		assertEquals(ApplicantName.INVALID_NAME, name.validateName());
+	}
+	
+	@Test
+	public void missingLastName() {
+		name.setName("First", null, null);
+		assertEquals(ApplicantName.INVALID_NAME, name.validateName());
+	}
+	
+	@Test
+	public void completeSpanishNameProvided() {
+		name.setSpanishName("PrimerNombre", "SegundoNombre", "PrimerApellido", "SegundoApellido");
+		assertEquals(ApplicantName.VALID_NAME, name.validateName());
+	}
+	
+	@Test
+	public void spanishNameWithOneFirstNameProvided() {
+		name.setSpanishName("PrimerNombre", null, "PrimerApellido", "SegundoApellido");
+		assertEquals(ApplicantName.VALID_NAME, name.validateName());
+	}
+	
+	@Test
+	public void spanishNameWithOneLastNameProvided() {
+		name.setSpanishName("PrimerNombre", null, "PrimerApellido", null);
+		assertEquals(ApplicantName.VALID_NAME, name.validateName());
+	}
+	
+	@Test
+	public void spanishNameWithNoFirstNameProvided() {
+		name.setSpanishName(null, null, "PrimerApellido", null);
+		assertEquals(ApplicantName.INVALID_NAME, name.validateName());
+	}
+	
+	@Test
+	public void spanishNameWithNoLastNameProvided() {
+		name.setSpanishName("PrimerNombre", "SegundoNombre", null, null);
+		assertEquals(ApplicantName.INVALID_NAME, name.validateName());
+	}
+	
+	@Test
+	public void formatEnglishNameLastNameFirst() {
+		name.setName("First", "Middle", "Last");
+		assertEquals("Last, First Middle", name.formatLastNameFirst());
+	}
+	
 
 }
