26b3b3a75cf0909eec67693ef6bc42835bccc561
Dave Haws <davehaws@users.noreply.github.com>
2018-12-01 20:56:00 -0600

    Replaced zip code in JobApplicant with ZipCode class.

--- src/main/java/com/neopragma/legacy/screen/JobApplicant.java
+++ src/main/java/com/neopragma/legacy/screen/JobApplicant.java
@@ -1,19 +1,9 @@
 package com.neopragma.legacy.screen;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Scanner;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
 /**
  * Job applicant class.
@@ -25,10 +15,8 @@ public class JobApplicant {
 	private String lastName = null;
 	
 	private SocialSecurityNumber ssn;
-
-	private String zipCode;    
-	private String city;
-	private String state;
+	
+	private ZipCode zipCode;    
 
 	public void setName(String firstName, String middleName, String lastName) {
 		this.firstName = firstName == null ? "" : firstName;
@@ -80,82 +68,16 @@ public class JobApplicant {
 		return ssn.getSsnState();
 	}
 
-	public void setZipCode(String zipCode) throws URISyntaxException, IOException {
-		this.zipCode = zipCode;
-		
-		String cityStateContent = getCityStateContentFromZipCode();
-        
-        if (cityStateContent != null) {
-        	int cityIndex = getIndexOfCityInContent(cityStateContent);
-        	int cityEnd = cityStateContent.indexOf(" ", cityIndex);
-        	city = cityStateContent.substring(cityIndex, cityEnd);
-        	
-        	int stateIndex = cityEnd+1;
-        	int stateEnd = stateIndex+2;
-        	state = cityStateContent.substring(stateIndex, stateEnd);
-        }
-	}
-
-	private int getIndexOfCityInContent(String cityStateContent) {
-		int currentOffset = 0;
-		String[] contentSections = new String[]{"<meta ", " content=\"Zip Code ", " - "};
-		
-		for (String section : contentSections) {
-			currentOffset = cityStateContent.indexOf(section, currentOffset);
-			currentOffset += section.length();
-		}
-		return currentOffset;
-	}
-
-	private String getCityStateContentFromZipCode() throws URISyntaxException, IOException, ClientProtocolException {
-		String cityStateContent = null;
-		
-		CloseableHttpResponse response = getCityStateResponseFromZipCodeService();
-        try {
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                cityStateContent = getContentFromResponse(response, entity);
-            }
-        } finally {
-            response.close();
-        }
-		return cityStateContent;
-	}
-
-	private String getContentFromResponse(CloseableHttpResponse response, HttpEntity entity) throws IOException {
-		long len = entity.getContentLength();
-		BufferedReader rd = new BufferedReader(
-		        new InputStreamReader(response.getEntity().getContent()));
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
-		return result.toString();
-	}
-
-	private CloseableHttpResponse getCityStateResponseFromZipCodeService()
-			throws URISyntaxException, IOException, ClientProtocolException {
-		URI uri = new URIBuilder()
-            .setScheme("http")
-            .setHost("www.zip-codes.com")
-            .setPath("/search.asp")
-            .setParameter("fld-zip", this.zipCode)
-            .setParameter("selectTab", "0")
-            .setParameter("srch-type", "city")
-            .build();
-        HttpGet request = new HttpGet(uri);
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        CloseableHttpResponse response = httpclient.execute(request);
-		return response;
+	public void setZipCode(String zip) throws URISyntaxException, IOException {
+		zipCode = new ZipCode(zip);
 	}
 
 	public String getCity() {
-		return city;
+		return zipCode.getCity();
 	}
 
 	public String getState() {
-		return state;
+		return zipCode.getState();
 	}
 	
 	public void add(String firstName,
